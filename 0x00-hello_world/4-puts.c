abb1d60a6119f156422dbf7c6600d85b4580ab62
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-09-09 03:45:10 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - prints the string in the puts funtion
-*
-*Description: using the main funtion
-*this program prints \"Programming is like building a multilingual puzzle
-*return: 0
-*/
+ * main - prints the string in the puts funtion
+ *
+ * Description: using the main funtion
+ * this program prints \"Programming is like building a multilingual puzzle
+ * return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
