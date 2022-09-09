0dd6d98765799894174b9c5bd10a46fc3570bce3
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-09-09 12:58:40 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - prints the string of the function\
+ *
+ * Description: uses the main argument
+ * This program prints exactly the imput to the
+ * standar error
+ * Return: 1
+ */
+int main(void)
+{
+	char err[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
