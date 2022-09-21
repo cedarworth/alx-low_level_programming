1f00cb8c37e32fa1b67a1f17385179f1163b9f72
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-09-21 22:27:19 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  *_strncpy - C function that copies a string, including the
@@ -20,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for (i = 0; i < n; i++)
+	for (i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
