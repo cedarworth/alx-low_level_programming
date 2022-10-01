013992fc417534249eb2ae89ae4c1dc561759ca7
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-01 02:55:54 +0100

    update

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,17 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
+
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
- * check_num prototype
- * _putchar prototype
+ * _putchar - writes character c to stdou
+ * check_num prototypet
+ * @c: the character to be printed
+ *
+ * Return: on success 1
+ * on error, -1 is returned
  */
 int _putchar(char c);
 int check_num(char *str);
