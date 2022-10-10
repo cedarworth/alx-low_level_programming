01708b3e0e994ba8160229784e3eb1cb0d8b3cd8
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-10 23:28:10 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
-0. Define a new type struct dog with the following elements
-1. Write a function that initialize a variable of type struct dog
-2. Write a function that prints a struct dog
-3. Define a new type dog_t as a new name for the type struct dog.
-4. Write a function that creates a new dog.
-5. Write a function that frees dogs.
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
