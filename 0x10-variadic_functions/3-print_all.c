870f2a7831c4a150713be5e62e4a6db6a9dc731e
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-14 22:41:12 +0100

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,8 +3,8 @@
 #include "variadic_functions.h"
 
 /**
- * print_c - prints all character arguments
- * @c: character to be printed
+ * print_c - print a char
+ * @c: char to print
  *
  * Return: void
  */
@@ -15,7 +15,7 @@ void print_c(va_list c)
 
 /**
  * print_s - prints a string
- * @s: string to be printed
+ * @s: string to print
  *
  * Return: void
  */
@@ -30,11 +30,63 @@ void print_s(va_list s)
 
 /**
  * print_i - prints an int
- * @i: int to be printed
+ * @i: int to print
  *
  * Return: void
  */
 void print_i(va_list i)
 {
-	printf("%");
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
+
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (p[j].t != NULL)
+		{
+			if (*(p[j].t) == format[i])
+			{
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(valist);
+	printf("\n");
 }
