ddd6d9161607164888abc93aabd8eece9ebdd532
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-14 04:33:29 +0100

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,7 +11,7 @@ typedef struct print
 {
 	char *t;
 	void (*f)(va_list);
-}print_t;
+} print_t;
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
