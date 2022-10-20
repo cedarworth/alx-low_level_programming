b2efbadf44a7c58eca38db473207e3451ed78a35
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-20 21:26:10 +0100

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef _liSTS_
+#ifndef _LISTS_
 #define _LISTS_
 
 #include <stdio.h>
@@ -6,7 +6,7 @@
 #include <string.h>
 
 /**
- *struct lists - singly linked list
+ *struct list_s - singly linked list
  *@str: string - (malloc'ed string)
  *@len: length of the string
  *@next: points to the next node
@@ -25,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif /* _liSTS_ */
