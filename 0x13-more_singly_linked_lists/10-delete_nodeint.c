103b26d8c44c71c3a7102ffe97066e16bd8f3917
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-25 21:48:55 +0100

    Delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(copy);
-		return (-1);
+		return (1);
 	}
 	for (node = 0; node < (index - 1); node++)
 	{
