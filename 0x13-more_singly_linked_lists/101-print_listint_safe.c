086ee74a781b085098ca3764736b6a0646352735
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-26 12:46:26 +0100

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -53,7 +53,7 @@ size_t print_listint_safe(const listint_t *head)
 			add = add->next;
 			if (head == add->p)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n)
+				printf("-> [%p] %d\n", (void *)head, head->n);
 				free_listp(&hptr);
 				return (nnodes);
 			}
