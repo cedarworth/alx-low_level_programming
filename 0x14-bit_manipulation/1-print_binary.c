fa8490c01af42c6c95f31930883385d22be95dd4
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-27 17:26:23 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "main.h"i
+#include "main.h"
 
 /**
  *print_binary - prints the binary representation
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
