935147aa5d336d7aa1954094c2d332acf3912509
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-10-27 16:15:46 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"i
 
 /**
  *print_binary - prints the binary representation
@@ -14,7 +13,7 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-			_putchar((n & 1) + '0');
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
