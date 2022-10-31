48910a5a88afb75f1b92ea26efc0d0d58b51b150
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-11-01 00:41:55 +0100

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -36,7 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	lenw = wrte(STDOUT_FILENO, buffer, lenr);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
 	if (lenr != lenw)
 		return (0);
