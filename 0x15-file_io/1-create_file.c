a3e5bc35c5e4891d45062c3763da30bfbe0d03fe
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-11-01 17:38:36 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,5 +21,17 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	:wq
-
+	if (!text_content)
+		text_content = "";
+	
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr =write (fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+	
+	close(fd);
+	return (1);
+}
