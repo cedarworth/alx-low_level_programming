7204571083d8f47a3bc77adef5a4c8012329bae6
Tunde Babatunde <tundemichaelbabatunde@gmail.com>
2022-11-02 21:55:17 +0100

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *create_file - creates a file
- *@filename: filename
- *@text_content: contents written in the file
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- *Return: 1 if it suceeds, -1 if not
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if (!filename0
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,5 +21,18 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	:wq
-
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
